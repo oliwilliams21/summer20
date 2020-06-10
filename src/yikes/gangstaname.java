d471db48987ff460784d90917c4f31f8ae3ed3e0
oli <williamsd21@greenhill.org>
2020-06-10 09:56:52 -0500

    edited gangstaname

--- src/yikes/gangstaname.java
+++ src/yikes/gangstaname.java
@@ -2,9 +2,9 @@ package yikes;
 import java.util.Scanner;
 public class gangstaname {
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.print("Type your name, playa: ");
-        String name = console.nextLine();
+        String name = input.nextLine();
 // split name into first/last name and initials
         String first = name.substring(0, name.indexOf(" "));
         String last = name.substring(name.indexOf(" ") + 1);
