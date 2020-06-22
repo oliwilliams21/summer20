c50a3ef3a60ac2b18582e65b30272ec81575f805
oli <williamsd21@greenhill.org>
2020-06-22 08:39:18 -0500

    challenge 5 method

--- src/yikes/stringchallenge.java
+++ src/yikes/stringchallenge.java
@@ -5,19 +5,22 @@ import java.util.*;
 //return method - have one
 //separate even and odd indexed numbers
 //print on separate lines
-//public class stringchallenge {
-//    public static void main (String[] args){
-//        Scanner input = new Scanner(System.in);
-//        System.out.print("input phrase");
-//        String wholephrase = input.nextLine();
-//
-//
-//        if(index%2=1){
-//            String odd ;
-//        } else if(index%2=0){
-//            String even ;
-//        }
-//        for(int odd=1; odd+=2){
-//            System.out.print(" "+odd);
-//    }
-//}}
+public class stringchallenge {
+    public static void main (String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("input word: ");
+        String wholephrase = input.nextLine();
+        String odd="";
+        String even="";
+        for(int oddindex=1; oddindex<=wholephrase.length(); oddindex+=2){
+                odd= odd+wholephrase.charAt(oddindex);
+        }
+        System.out.println();
+        for(int evenindex=0; evenindex<=wholephrase.length(); evenindex+=2) {
+            even= even+wholephrase.charAt(evenindex);
+        }
+        String answer = even+" "+odd;
+        System.out.print(answer);
+        }
+}
